3aa140922979271a73454e1bdf48686462f9944d
Carlos <karloscerbero93@hotmail.com>
2021-06-11 15:23:58 +0200

    v BUENA

--- index.js
+++ index.js
@@ -37,10 +37,12 @@ app.use(express.static('public'));
 //agregar router
 app.use('/', router);
 
-const port = process.env.PORT || 4000 ;
+const port = process.env.PORT || 4000;
 const host = process.env.HOST || '0.0.0.0';
 
-
-app.listen (port, host, ()=>{
+app.listen (port, ()=>{
+    console.log('el puerto esta funcionando')
+});
+app.listen (host, ()=>{
     console.log('El servidor esta funcionando')
-})
+}); 
