7a0fe2756293bf846a18d02ca114a634d7b3f8c6
Carlos <karloscerbero93@hotmail.com>
2021-06-11 13:55:14 +0200

    por favor que funcione eliminando el puerto

--- index.js
+++ index.js
@@ -37,10 +37,10 @@ app.use(express.static('public'));
 //agregar router
 app.use('/', router);
 
-const port = process.env.PORT;
+const port = process.env.PORT || 4000;
 const host = process.env.HOST || '0.0.0.0';
 
 
 app.listen (port, host, ()=>{
-    console.log(`El servidor esta funcionando en ${PORT}`)
+    console.log(`El servidor esta funcionando en ${port}`)
 })
